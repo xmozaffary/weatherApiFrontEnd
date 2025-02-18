df05dba36688fad272b198c9b5ca1ccdd4e540e0
Ali Rezai <ali.rezai27461@gmail.com>
2025-02-18 22:03:26 +0100

    service

---
+++ src/model/ICoordinated.ts
@@ -0,0 +1,7 @@
+export interface ICoordinates {
+  name: string;
+  localNames: [];
+  lat: number;
+  lon: number;
+  country: string;
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
+import { get } from "../service/serviceBase";
+import { ICoordinates } from "../model/ICoordinated";
+
 export const Home = () => {
+  const [coordinates, seyCoordinates] = useState<ICoordinates[] | null>(null);
+
+  useEffect(() => {
+    const getCoordinates = async () => {
+      const res = await get<ICoordinates[]>(
+        "http://localhost:8080/coordinates/london"
+      );
+
+      seyCoordinates(res);
+    };
+
+    console.log("test");
+
+    getCoordinates();
+  }, []);
+
   return (
     <>
       <main className="home">
-        <div className="hero-img">
-          <div className="hero-text">
-            <p>lorem ipsom</p>
-          </div>
-        </div>
+        {coordinates && (
+          <>
+            <p>{coordinates[0]?.name}</p>
+            <p>{coordinates[0]?.country}</p>
+          </>
+        )}
       </main>
     </>
   );
